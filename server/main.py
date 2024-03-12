197f66293689785aa7123829811f461174874a45
hosenur <nur08439@gmail.com>
2024-03-13 00:39:47 +0530

    Changed from tensorflow to pytorch

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
+from chat import get_response
 from fastapi.middleware.cors import CORSMiddleware
-from chatbot import chatbot
 origins = [
         "http://localhost:5173",
         "http://127.0.0.1:5173"
@@ -21,7 +21,6 @@ def read_root():
 @app.post("/chat")
 async def chat(request: Request):
     data = await request.json()
-    print(request)
     message = data['message']
-    result = chatbot(message)
+    result = get_response(message)
     return {"response": result}
